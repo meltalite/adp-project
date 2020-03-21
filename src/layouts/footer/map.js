737831ecaeac493be4c3139733a12a218b12cbcb
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-03-21 18:30:05 +0700

    Implement map section

---
+++ src/layouts/footer/map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './map.scss';
+
+export default function Map() {
+    return (
+        <div className="row" >
+            <div className="col-0 col-md-6" ></div>
+            <div className="map col-12 col-md-6" ></div>
+        </div>
+    )
+}
--- src/layouts/home.js
+++ src/layouts/home.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Navigation from "./navigation/navigation";
 import { NavigationItems } from './navigation/navigation-items';
+import Map from './footer/map';
 
 export default function Home() {
+    const style = {
+        marginBottom: '20px',
+        padding: '50px 0' 
+    }
     return (
-       <>
-       <Navigation></Navigation>
-       <h1>HOI</h1>
-       {NavigationItems.map(item => (
-           <div id={item.goTo} style={{ marginTop: '40px', marginBottom: '1000px', padding: '50px 0'}}>
-               <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra nibh ac libero lobortis dictum. Vestibulum at nulla et ligula placerat vulputate. Phasellus lacus mi, molestie sit amet turpis vel, maximus faucibus arcu. Suspendisse potenti. Fusce ut tincidunt diam. Vivamus sed pharetra orci. In hac habitasse platea dictumst. In quis leo vel turpis mollis ornare. Vestibulum ac volutpat risus, eget consectetur erat. Maecenas malesuada purus et nibh dapibus, eget ornare justo faucibus.
-               </p>
-           </div>
-       ))}
-       </> 
+        <div className="container">
+            <Navigation></Navigation>
+            <h1>HOI</h1>
+            {NavigationItems.map(item => (
+                <div key={item.goTo} className="row" >
+                    <section id={item.goTo} style={style}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra nibh ac libero lobortis dictum. Vestibulum at nulla et ligula placerat vulputate. Phasellus lacus mi, molestie sit amet turpis vel, maximus faucibus arcu. Suspendisse potenti. Fusce ut tincidunt diam. Vivamus sed pharetra orci. In hac habitasse platea dictumst. In quis leo vel turpis mollis ornare. Vestibulum ac volutpat risus, eget consectetur erat. Maecenas malesuada purus et nibh dapibus, eget ornare justo faucibus.
+                        </p>
+                    </section>
+                </div>
+            ))}
+            <Map></Map>
+       </div> 
     )
 }
