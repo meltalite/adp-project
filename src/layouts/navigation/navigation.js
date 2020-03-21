cb36700beed6acdd9626fcd4fa93d2cfc96c8bda
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-03-21 18:31:27 +0700

    Implement Navigation to adaptive to mobile view

--- src/layouts/navigation/navigation.js
+++ src/layouts/navigation/navigation.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navigation.scss'
-import logo from '../../logo.svg';
-import { Navbar, Nav, NavItem } from 'reactstrap';
+import { Navbar, Nav, NavItem, NavbarBrand, NavbarToggler, Collapse } from 'reactstrap';
 import { NavigationItems } from './navigation-items';
 
 export default function Navigation() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => setIsOpen(!isOpen)
     return (
-        <Navbar light sticky fixed={'top'}>
-            <a href={'#'} >
-                <img src={logo} alt={'logo'} className={'logo'} />
-            </a>
-            <Nav className={'main-nav'} justified={true} >
-                {
-                    NavigationItems.map(navItem => (
-                        <NavItem style={{ marginRight: '20px' }} >
-                            <a href={`#${navItem.goTo}`}>{navItem.text}</a>
-                        </NavItem>
-                    ))
-                }
+        <>
+        <Navbar light fixed='top'>
+            <NavbarBrand href='#'>ADP</NavbarBrand>
+            <NavbarToggler id='navbarMobile' className='nav-toggler d-block d-sm-none' onClick={toggle}/>
+            <Collapse isOpen={isOpen} navbar className='d-sm-none' >
+                    <Nav className='main-nav d-block d-sm-none' justified>
+                        {
+                            NavigationItems.map(navItem => (
+                                <NavItem key={navItem.goTo} style={{ marginRight: '20px' }} className='d-block d-sm-none text-uppercase'>
+                                    <a href={`#${navItem.goTo}`}>{navItem.text}</a>
+                                </NavItem>
+                            ))
+                        }
+                    </Nav>
+            </Collapse>
+            <Nav id='navbarDesktop' className='main-nav d-none d-lg-flex' >
+                    {
+                        NavigationItems.map(navItem => (
+                            <NavItem key={navItem.goTo} style={{ marginRight: '20px' }} className='d-none d-lg-flex text-uppercase'>
+                                <a href={`#${navItem.goTo}`}>{navItem.text}</a>
+                            </NavItem>
+                        ))
+                    }
             </Nav>
         </Navbar>
+        </>
     )
 }
