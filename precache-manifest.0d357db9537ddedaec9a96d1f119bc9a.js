7c1f2856fb1b0a1d959dc7926a2eb790a6880ea7
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-03-24 22:26:26 +0700

    Updates

--- precache-manifest.0d357db9537ddedaec9a96d1f119bc9a.js
+++ precache-manifest.0d357db9537ddedaec9a96d1f119bc9a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "958a144905fbc7ee3d76b636107b4874",
+    "revision": "30ee09c646bdb41bc48ed84586d577cd",
     "url": "/adp-project/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/adp-project/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "ec54eb1f44845bf42c91",
-    "url": "/adp-project/static/css/main.f3e5e794.chunk.css"
+    "revision": "3dbc9ec251f0a73427cd",
+    "url": "/adp-project/static/css/main.cf424743.chunk.css"
   },
   {
     "revision": "29c64a873f94bbad095e",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/adp-project/static/js/2.2f52230c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ec54eb1f44845bf42c91",
+    "revision": "3dbc9ec251f0a73427cd",
     "url": "/adp-project/static/js/main.e9612963.chunk.js"
   },
   {
