ad65bad0c29cf84c1d35c81fa504d02830b7ef73
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-03-25 15:28:39 +0700

    resize map temporary

--- src/layouts/footer/map.js
+++ src/layouts/footer/map.js
@@ -5,7 +5,7 @@ export default function Map() {
     return (
         <div className="row" >
             <div className="col-0 col-md-6" ></div>
-            <div className="map col-12 col-md-6" ></div>
+            <div className="map col-12 col-md-4" ></div>
         </div>
     )
 }
