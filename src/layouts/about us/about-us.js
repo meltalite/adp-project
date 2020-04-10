34bc2879d8c19376cf97bcbf27fde05d98000ef8
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-04-10 17:34:43 +0700

    update about us css

--- src/layouts/about us/about-us.js
+++ src/layouts/about us/about-us.js
@@ -20,28 +20,38 @@ const items = [
 export default function AboutUs() {
   const { t } = useTranslation()
 
-  const pictures = items.map(item => {
-    return (
-      <img src={item.src} alt={item.altText} key={item.altText} />
+  const pictures = items.map((item, index) => {
+    return (index === 0 ?
+      (
+        <div className='col-12' >
+        <img src={item.src} alt={item.altText} key={item.altText} />
+        </div>
+      ) : (
+        <div className='col-6' >
+        <img src={item.src} alt={item.altText} key={item.altText} />
+        </div>
+      )
     )
   })
 
   return (
     <section className='page-section'>
-      <div className='about-us' id={encodeURI(t('Nav.about us'))}>
-        <div className='col-12 col-md-5 article' >
-          <h2>
-            {t('About Us.title')}
-          </h2>
-          <p>
-            {t('About Us.p1')}
-          </p>
-          <p>
-            {t('About Us.p2')}
-          </p>
-        </div>
-        <div className='col-12 col-md-5 album' >
-          {pictures}
+      <div className='container'>
+        <div className='about-us' id={encodeURI(t('Nav.about us'))}>
+          <div className='col-12 col-lg-5 article' >
+            <h2>
+              {t('About Us.title')}
+            </h2>
+            <p>
+              {t('About Us.p1')}
+            </p>
+            <p>
+              {t('About Us.p2')}
+            </p>
+          </div>
+          <div className='col-12 col-lg-5 album' >
+            {pictures}
+          </div>
         </div>
       </div>
     </section>
