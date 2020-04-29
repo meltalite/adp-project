2978aa6e27f08db7cdaa6c51db4409797163692b
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-04-29 07:11:28 +0700

    delete image in section contact us

--- src/layouts/contact us/contact-us.js
+++ src/layouts/contact us/contact-us.js
@@ -3,37 +3,8 @@ import './contact-us.scss';
 import { useTranslation } from 'react-i18next';
 import EmailForm from './email/email-form';
 
-const items = [
-  {
-    src: `${process.env.PUBLIC_URL}/pictures/contact us/cu_cs.jpg`,
-    altText: 'cu_cs',
-  },
-  {
-    src: `${process.env.PUBLIC_URL}/pictures/contact us/cu_polymer.jpg`,
-    altText: 'cu_polymer',
-  },
-  {
-    src: `${process.env.PUBLIC_URL}/pictures/contact us/cu_granules.jpg`,
-    altText: 'cu_granules',
-  },
-]
-
 export default function ContacUs() {
   const { t } = useTranslation()
-
-  const pictures = items.map((item, index) => {
-    return (index === 0 ?
-      (
-        <div className='col-12' key={item.altText} >
-        <img src={item.src} alt={item.altText} />
-        </div>
-      ) : (
-        <div className='col-6' key={item.altText} >
-        <img src={item.src} alt={item.altText} />
-        </div>
-      )
-    )
-  })
 
   return (
     <section className='page-section'>
@@ -44,9 +15,6 @@ export default function ContacUs() {
               {t('Contact Us.title')}
             </h2>
             <EmailForm />
-          </div>
-          <div className='col-12 col-lg-5 album' >
-            {pictures}
           </div>
         </div>
       </div>
