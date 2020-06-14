e9e2aaa8892e255df00d480ff30ef0c803708166
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-06-14 10:51:08 +0700

    update assets naming in engineering plastic

--- src/layouts/products/commodity-list.js
+++ src/layouts/products/commodity-list.js
@@ -3,16 +3,16 @@ const commodityPlastic = [
     name: 'HIPS',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hips_tumblr.jpg`,
-        altText: 'hips_tumblr'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hips_1.jpg`,
+        altText: 'hips_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hips_plastic.jpg`,
-        altText: 'hips_plastic'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hips_2.jpg`,
+        altText: 'hips_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hips_toilet.jpg`,
-        altText: 'hips_toilet'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hips_3.jpg`,
+        altText: 'hips_3'
       },
     ]
   },
@@ -20,16 +20,16 @@ const commodityPlastic = [
     name: 'GPPS',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/gpps_tray.jpg`,
-        altText: 'gpps_tray'
+        src: `${process.env.PUBLIC_URL}/pictures/products/gpps_1.jpg`,
+        altText: 'gpps_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/gpps_container.jpg`,
-        altText: 'gpps_container'
+        src: `${process.env.PUBLIC_URL}/pictures/products/gpps_2.jpg`,
+        altText: 'gpps_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/gpps_case.jpg`,
-        altText: 'gpps_case'
+        src: `${process.env.PUBLIC_URL}/pictures/products/gpps_3.jpg`,
+        altText: 'gpps_3'
       },
     ]
   },
@@ -37,16 +37,16 @@ const commodityPlastic = [
     name: 'EPS',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/eps_board.jpg`,
-        altText: 'eps_board'
+        src: `${process.env.PUBLIC_URL}/pictures/products/eps_1.jpg`,
+        altText: 'eps_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/eps_container.jpg`,
-        altText: 'eps_container'
+        src: `${process.env.PUBLIC_URL}/pictures/products/eps_2.jpg`,
+        altText: 'eps_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/eps_stack.jpg`,
-        altText: 'eps_stack'
+        src: `${process.env.PUBLIC_URL}/pictures/products/eps_3.jpg`,
+        altText: 'eps_3'
       },
     ]
   },
@@ -54,16 +54,16 @@ const commodityPlastic = [
     name: 'HPP',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hpp_string.jpg`,
-        altText: 'hpp_string'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hpp_1.jpg`,
+        altText: 'hpp_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hpp_roll.jpg`,
-        altText: 'hpp_roll'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hpp_2.jpg`,
+        altText: 'hpp_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hpp_valve.jpg`,
-        altText: 'hpp_valve'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hpp_3.jpg`,
+        altText: 'hpp_3'
       },
     ]
   },
@@ -71,16 +71,16 @@ const commodityPlastic = [
     name: 'BCPP',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/bcpp_wheel.jpg`,
-        altText: 'bcpp_wheel'
+        src: `${process.env.PUBLIC_URL}/pictures/products/bcpp_1.jpg`,
+        altText: 'bcpp_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/bcpp_container.jpg`,
-        altText: 'bcpp_container'
+        src: `${process.env.PUBLIC_URL}/pictures/products/bcpp_2.jpg`,
+        altText: 'bcpp_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/bcpp_pad.jpg`,
-        altText: 'bcpp_pad'
+        src: `${process.env.PUBLIC_URL}/pictures/products/bcpp_3.jpg`,
+        altText: 'bcpp_3'
       },
     ]
   },
@@ -88,16 +88,16 @@ const commodityPlastic = [
     name: 'HDPE',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hdpe_chair.jpg`,
-        altText: 'hdpe_chair'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hdpe_1.jpg`,
+        altText: 'hdpe_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hdpe_bottle.jpg`,
-        altText: 'hdpe_bottle'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hdpe_2.jpg`,
+        altText: 'hdpe_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/hdpe_gallon.jpg`,
-        altText: 'hdpe_gallon'
+        src: `${process.env.PUBLIC_URL}/pictures/products/hdpe_3.jpg`,
+        altText: 'hdpe_3'
       },
     ]
   },
@@ -105,16 +105,16 @@ const commodityPlastic = [
     name: 'LDPE',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/ldpe_slide.jpg`,
-        altText: 'ldpe_slide'
+        src: `${process.env.PUBLIC_URL}/pictures/products/ldpe_1.jpg`,
+        altText: 'ldpe_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/ldpe_container.jpg`,
-        altText: 'ldpe_container'
+        src: `${process.env.PUBLIC_URL}/pictures/products/ldpe_2.jpg`,
+        altText: 'ldpe_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/ldpe_wrapper.jpg`,
-        altText: 'ldpe_wrapper'
+        src: `${process.env.PUBLIC_URL}/pictures/products/ldpe_3.jpg`,
+        altText: 'ldpe_3'
       },
     ]
   },
@@ -122,16 +122,16 @@ const commodityPlastic = [
     name: 'LLDPE',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/lldpe_bag.jpg`,
-        altText: 'lldpe_bag'
+        src: `${process.env.PUBLIC_URL}/pictures/products/lldpe_1.jpg`,
+        altText: 'lldpe_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/lldpe_container.jpg`,
-        altText: 'lldpe_container'
+        src: `${process.env.PUBLIC_URL}/pictures/products/lldpe_2.jpg`,
+        altText: 'lldpe_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/lldpe_wrapper.jpg`,
-        altText: 'lldpe_wrapper'
+        src: `${process.env.PUBLIC_URL}/pictures/products/lldpe_3.jpg`,
+        altText: 'lldpe_3'
       },
     ]
   },
@@ -139,16 +139,16 @@ const commodityPlastic = [
     name: 'PVC',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/pvc_pipe.jpg`,
-        altText: 'pvc_pipe'
+        src: `${process.env.PUBLIC_URL}/pictures/products/pvc_1.jpg`,
+        altText: 'pvc_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/pvc_floor.jpg`,
-        altText: 'pvc_floor'
+        src: `${process.env.PUBLIC_URL}/pictures/products/pvc_2.jpg`,
+        altText: 'pvc_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/pvc_window.jpg`,
-        altText: 'pvc_window'
+        src: `${process.env.PUBLIC_URL}/pictures/products/pvc_3.jpg`,
+        altText: 'pvc_3'
       },
     ]
   },
@@ -156,16 +156,16 @@ const commodityPlastic = [
     name: 'PET',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/pet_bottle.jpg`,
-        altText: 'pet_bottle'
+        src: `${process.env.PUBLIC_URL}/pictures/products/pet_1.jpg`,
+        altText: 'pet_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/pet_rope.jpg`,
-        altText: 'pet_rope'
+        src: `${process.env.PUBLIC_URL}/pictures/products/pet_2.jpg`,
+        altText: 'pet_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/pet_utensil.jpg`,
-        altText: 'pet_utensil'
+        src: `${process.env.PUBLIC_URL}/pictures/products/pet_3.jpg`,
+        altText: 'pet_3'
       },
     ]
   },
@@ -173,16 +173,16 @@ const commodityPlastic = [
     name: 'EVA',
     pictures: [
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/eva_shoe.jpg`,
-        altText: 'eva_shoe'
+        src: `${process.env.PUBLIC_URL}/pictures/products/eva_1.jpg`,
+        altText: 'eva_1'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/eva_saddle.jpg`,
-        altText: 'eva_saddle'
+        src: `${process.env.PUBLIC_URL}/pictures/products/eva_2.jpg`,
+        altText: 'eva_2'
       },
       {
-        src: `${process.env.PUBLIC_URL}/pictures/products/eva_line.jpg`,
-        altText: 'eva_line'
+        src: `${process.env.PUBLIC_URL}/pictures/products/eva_3.jpg`,
+        altText: 'eva_3'
       },
     ]
  },
