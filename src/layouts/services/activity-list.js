434ed8044fa6b12bc06c189de1c2726ae968f6c9
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-06-17 20:47:43 +0700

    update assets for services

--- src/layouts/services/activity-list.js
+++ src/layouts/services/activity-list.js
@@ -20,16 +20,16 @@ const logisticService = {
   name: 'Logistic Service',
   pictures: [
     {
-      src: `${process.env.PUBLIC_URL}/pictures/services/ls_credit-card.jpg`,
-      altText: 'ls_credit-card'
+      src: `${process.env.PUBLIC_URL}/pictures/services/ls_1.jpg`,
+      altText: 'ls_1-card'
     },
     {
-      src: `${process.env.PUBLIC_URL}/pictures/services/ls_courier.jpg`,
-      altText: 'ls_courier'
+      src: `${process.env.PUBLIC_URL}/pictures/services/ls_2.jpg`,
+      altText: 'ls_2'
     },
     {
-      src: `${process.env.PUBLIC_URL}/pictures/services/ls_forklift.jpg`,
-      altText: 'ls_forklift'
+      src: `${process.env.PUBLIC_URL}/pictures/services/ls_3.jpg`,
+      altText: 'ls_3'
     },
   ]
 }
