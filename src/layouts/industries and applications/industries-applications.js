4fbbc98c4588b51a325af77d3eed383970d0abd5
Muhammad Mufti Muflihun <meltalite@gmail.com>
2020-10-07 19:57:32 +0700

    fix header in section industries & application got translated

--- src/layouts/industries and applications/industries-applications.js
+++ src/layouts/industries and applications/industries-applications.js
@@ -53,7 +53,7 @@ export default function IndustriesApplications() {
   const Header = () => {
     return (
       <div className='header'>
-        <h2>{t('Nav.industries & applications')}</h2>
+        <h2>{t('Industries & Applications.title')}</h2>
         <div className='header-tab'>
           {(tabs)}
         </div>
