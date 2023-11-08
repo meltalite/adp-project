bc8850c4b6d11fbc166386e33e2efb64874aa23c
M Mufti Muflihun <muhammad.muflihun@vidio.com>
2023-11-09 05:14:31 +0700

    Updates

---
+++ precache-manifest.0eabb4d6666cb87ad34d2e276cb2b253.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91903cd4e1e3cbaf60ecc43040aa3dd",
+    "url": "/adp-project/index.html"
+  },
+  {
+    "revision": "fdf8ed440f57f2dc89b2",
+    "url": "/adp-project/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "1284a060ab79e0a340a9",
+    "url": "/adp-project/static/css/main.d7d537bf.chunk.css"
+  },
+  {
+    "revision": "fdf8ed440f57f2dc89b2",
+    "url": "/adp-project/static/js/2.f228fd6a.chunk.js"
+  },
+  {
+    "revision": "6d1c2bc9c130665022fd05ad12bdb1f5",
+    "url": "/adp-project/static/js/2.f228fd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1284a060ab79e0a340a9",
+    "url": "/adp-project/static/js/main.b02e7658.chunk.js"
+  },
+  {
+    "revision": "65ee67c94d5226da3caf",
+    "url": "/adp-project/static/js/runtime-main.0164fff9.js"
+  },
+  {
+    "revision": "9e692ddf1feb3d111695b37f7949419d",
+    "url": "/adp-project/static/media/adp-white.9e692ddf.svg"
+  },
+  {
+    "revision": "b709add237a94857e80ffe1fa09c8d9f",
+    "url": "/adp-project/static/media/adp.b709add2.svg"
+  },
+  {
+    "revision": "f844374cf75d4caa2e199083b00812bb",
+    "url": "/adp-project/static/media/aerospace.f844374c.svg"
+  },
+  {
+    "revision": "ab6c59b6ac5926b08dd6d9d62ebc17e6",
+    "url": "/adp-project/static/media/automotive.ab6c59b6.svg"
+  },
+  {
+    "revision": "b0fdf3563b0e2fcef7aaf96f6aed0743",
+    "url": "/adp-project/static/media/cgoods.b0fdf356.svg"
+  },
+  {
+    "revision": "140396163c66ad249d587310a3e0504b",
+    "url": "/adp-project/static/media/ch.14039616.svg"
+  },
+  {
+    "revision": "151fe17a5643a69a9739d403d842cb13",
+    "url": "/adp-project/static/media/chemical.151fe17a.svg"
+  },
+  {
+    "revision": "ab7726c578479df610bc8ce6360af790",
+    "url": "/adp-project/static/media/electrical.ab7726c5.svg"
+  },
+  {
+    "revision": "87783b238cbefe577cde8def28245af4",
+    "url": "/adp-project/static/media/energy.87783b23.svg"
+  },
+  {
+    "revision": "58532549e65d51beab55b3a3887b2a26",
+    "url": "/adp-project/static/media/fb.58532549.svg"
+  },
+  {
+    "revision": "2af6dd492026e8b56bd257f27e9ce7a1",
+    "url": "/adp-project/static/media/healthcare.2af6dd49.svg"
+  },
+  {
+    "revision": "1d6a11bfe30caad595e9a26bed16755c",
+    "url": "/adp-project/static/media/ig.1d6a11bf.svg"
+  },
+  {
+    "revision": "f19ae7544e9048181d4d8df35b543203",
+    "url": "/adp-project/static/media/jp.f19ae754.svg"
+  },
+  {
+    "revision": "9c5a10eaf2af0b16b18f87c3c90a5319",
+    "url": "/adp-project/static/media/linkedin.9c5a10ea.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adp-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "79a5d07813a271a2a19ea070885b39ab",
+    "url": "/adp-project/static/media/magnifier.79a5d078.svg"
+  },
+  {
+    "revision": "fb7cff9629ac2d908782aed781f6849f",
+    "url": "/adp-project/static/media/map-desktop.fb7cff96.svg"
+  },
+  {
+    "revision": "41b0b5edfe520a95a6d5ae95574fca4a",
+    "url": "/adp-project/static/media/map-mobile.41b0b5ed.svg"
+  },
+  {
+    "revision": "d02e8f4e663378a468c4d150b0f29469",
+    "url": "/adp-project/static/media/twitter.d02e8f4e.svg"
+  },
+  {
+    "revision": "f656668bea33fc79f2691b70a1f883a3",
+    "url": "/adp-project/static/media/uk.f656668b.svg"
+  },
+  {
+    "revision": "3fdd466c819ab5953ce6c1e35801bf0b",
+    "url": "/adp-project/static/media/youtube.3fdd466c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53922366f098f26d5ed4191097fff237.js"
+  "/adp-project/precache-manifest.0eabb4d6666cb87ad34d2e276cb2b253.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adp-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
