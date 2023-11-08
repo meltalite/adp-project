cf98cf6814f6f59cbfca2154c7f47ea628e3dc60
M Mufti Muflihun <muhammad.muflihun@vidio.com>
2023-11-09 05:01:12 +0700

    Updates

---
+++ precache-manifest.53922366f098f26d5ed4191097fff237.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d33b113d7a6cc07706b5a706577ad0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdf8ed440f57f2dc89b2",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "143294dcc1978ed02cea",
+    "url": "/static/css/main.c7f07cfd.chunk.css"
+  },
+  {
+    "revision": "fdf8ed440f57f2dc89b2",
+    "url": "/static/js/2.f228fd6a.chunk.js"
+  },
+  {
+    "revision": "6d1c2bc9c130665022fd05ad12bdb1f5",
+    "url": "/static/js/2.f228fd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143294dcc1978ed02cea",
+    "url": "/static/js/main.51be5104.chunk.js"
+  },
+  {
+    "revision": "57c5f3c0536963b14e19",
+    "url": "/static/js/runtime-main.e756b764.js"
+  },
+  {
+    "revision": "9e692ddf1feb3d111695b37f7949419d",
+    "url": "/static/media/adp-white.9e692ddf.svg"
+  },
+  {
+    "revision": "b709add237a94857e80ffe1fa09c8d9f",
+    "url": "/static/media/adp.b709add2.svg"
+  },
+  {
+    "revision": "f844374cf75d4caa2e199083b00812bb",
+    "url": "/static/media/aerospace.f844374c.svg"
+  },
+  {
+    "revision": "ab6c59b6ac5926b08dd6d9d62ebc17e6",
+    "url": "/static/media/automotive.ab6c59b6.svg"
+  },
+  {
+    "revision": "b0fdf3563b0e2fcef7aaf96f6aed0743",
+    "url": "/static/media/cgoods.b0fdf356.svg"
+  },
+  {
+    "revision": "140396163c66ad249d587310a3e0504b",
+    "url": "/static/media/ch.14039616.svg"
+  },
+  {
+    "revision": "151fe17a5643a69a9739d403d842cb13",
+    "url": "/static/media/chemical.151fe17a.svg"
+  },
+  {
+    "revision": "ab7726c578479df610bc8ce6360af790",
+    "url": "/static/media/electrical.ab7726c5.svg"
+  },
+  {
+    "revision": "87783b238cbefe577cde8def28245af4",
+    "url": "/static/media/energy.87783b23.svg"
+  },
+  {
+    "revision": "58532549e65d51beab55b3a3887b2a26",
+    "url": "/static/media/fb.58532549.svg"
+  },
+  {
+    "revision": "2af6dd492026e8b56bd257f27e9ce7a1",
+    "url": "/static/media/healthcare.2af6dd49.svg"
+  },
+  {
+    "revision": "1d6a11bfe30caad595e9a26bed16755c",
+    "url": "/static/media/ig.1d6a11bf.svg"
+  },
+  {
+    "revision": "f19ae7544e9048181d4d8df35b543203",
+    "url": "/static/media/jp.f19ae754.svg"
+  },
+  {
+    "revision": "9c5a10eaf2af0b16b18f87c3c90a5319",
+    "url": "/static/media/linkedin.9c5a10ea.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "79a5d07813a271a2a19ea070885b39ab",
+    "url": "/static/media/magnifier.79a5d078.svg"
+  },
+  {
+    "revision": "fb7cff9629ac2d908782aed781f6849f",
+    "url": "/static/media/map-desktop.fb7cff96.svg"
+  },
+  {
+    "revision": "41b0b5edfe520a95a6d5ae95574fca4a",
+    "url": "/static/media/map-mobile.41b0b5ed.svg"
+  },
+  {
+    "revision": "d02e8f4e663378a468c4d150b0f29469",
+    "url": "/static/media/twitter.d02e8f4e.svg"
+  },
+  {
+    "revision": "f656668bea33fc79f2691b70a1f883a3",
+    "url": "/static/media/uk.f656668b.svg"
+  },
+  {
+    "revision": "3fdd466c819ab5953ce6c1e35801bf0b",
+    "url": "/static/media/youtube.3fdd466c.svg"
+  }
+]);
